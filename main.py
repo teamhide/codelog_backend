66bf81315ffe6c6f4e19c92d5344803e2f1d1dd1
Hide <padocon@naver.com>
2020-02-17 16:49:10 +0900

    Fix main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from app import create_app
+from core.settings import get_config
 
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=True, port=8000)
+    app.run(
+        host='0.0.0.0',
+        debug=False if get_config().env == 'production' else True,
+        port=8000,
+    )
